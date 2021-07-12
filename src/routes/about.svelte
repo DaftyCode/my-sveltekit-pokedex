a04dbe2f73fcea4ff397ce8dd48e647a12b2f04f
Rodrigo Martorell <juan-jose.rodrigo-martorell@capgemini.com>
2021-07-12 16:54:21 +0200

    Finish app without api endpoints

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>About the svelte kit pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p>This is a really cool svelte demo that uses the Pokemon API to build a Pokedex</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { pokemons } from "../stores/pokestore";
+    import PokemonCard from "../components/pokemon-card.svelte";
+
+    let searchTerm = "";
+    let filteredPokemon = [];
+
+    $: {
+        if (searchTerm) {
+            filteredPokemon = $pokemons.filter(pokemon => pokemon.name.toLowerCase()
+            .includes(searchTerm.toLowerCase()));
+        } else {
+            filteredPokemon = [...$pokemons];
+        }
+    }
+</script>
+<svelte:head>
+    <title>'Svelte Kit Pokedex'</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
+
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" 
+type="text" bind:value={searchTerm} placeholder="Search Pokemon">
+
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+    {#each filteredPokemon as pokemon}
+        <PokemonCard pokemon={pokemon} />
+    {/each}
+</div>
